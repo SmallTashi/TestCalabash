7d06b98c5b4e075bf49979e4a425b7561d23c202
thashi <xzmaoao123@outlook.com>
2018-03-01 02:01:07 +0800

    做好了点击事件绑定对应Fragment，icon更换有待优化，最好在Fragment中增加一个滑动监听类，以切换Fragment（这。。好像就是ViewPager与TabLayout联动。。吧）

--- app/src/main/java/com/tashi/testcalabash/MainActivity.java
+++ app/src/main/java/com/tashi/testcalabash/MainActivity.java
@@ -1,11 +1,9 @@
 package com.tashi.testcalabash;
 
-import android.annotation.SuppressLint;
-
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
-import android.support.design.widget.TabItem;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -13,11 +11,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.tashi.testcalabash.Fragment.FindFragment;
 import com.tashi.testcalabash.Fragment.PersonalFragmenr;
 import com.tashi.testcalabash.Fragment.RecommendFragment;
 import com.tashi.testcalabash.Fragment.ThemeFragment;
+
+import java.util.ArrayList;
 
 /*
 *
@@ -36,17 +38,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        final TabLayout tabLayout = findViewById(R.id.tabLayout);
+        RelativeLayout theme = findViewById(R.id.theme_item);
+        theme.setOnClickListener(this);
+        RelativeLayout recommend =findViewById(R.id.recommend_item);
+        recommend.setOnClickListener(this);
+        RelativeLayout find =findViewById(R.id.find_item);
+        find.setOnClickListener(this);
+        RelativeLayout personal =findViewById(R.id.personal_item);
+        personal.setOnClickListener(this);
         replaceFragment(new ThemeFragment());
-
-        final TabItem theme = findViewById(R.id.theme_item);
-        theme.setOnClickListener(this);
-        final TabItem recommend = findViewById(R.id.recommend_item);
-        recommend.setOnClickListener(this); //TODO:此处有BUG,解决办法（try）：创建一个事件监听类(不能再主线程？？)继承OnClickListener
-        final TabItem find = findViewById(R.id.find_item);
-        theme.setOnClickListener(this);
-        final TabItem personal = findViewById(R.id.personal_item);
-        theme.setOnClickListener(this);
     }
 
     private void replaceFragment(Fragment fragment) {
@@ -58,19 +58,64 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        //TODO : 图标更换太慢，改进方案——将八个icon初始化放到一个ArrayList里边（有序的）——优化速度？
+        ImageView theme = findViewById(R.id.q);
+        ImageView find = findViewById(R.id.as);
+        ImageView recommend = findViewById(R.id.e);
+        ImageView personal = findViewById(R.id.v);
         switch (v.getId()) {
             case R.id.theme_item:
+                recommend.setImageDrawable(getResources().getDrawable(R.mipmap.recommed_unsel));
+                theme.setImageDrawable(getResources().getDrawable(R.mipmap.theme_sel));
+                find.setImageDrawable(getResources().getDrawable(R.mipmap.follow_unsel));
+                personal.setImageDrawable(getResources().getDrawable(R.mipmap.mine_unsel));
                 replaceFragment(new ThemeFragment());
                 break;
             case R.id.recommend_item:
+                recommend.setImageDrawable(getResources().getDrawable(R.mipmap.recommend_sel));
+                theme.setImageDrawable(getResources().getDrawable(R.mipmap.theme_unsel));
+                find.setImageDrawable(getResources().getDrawable(R.mipmap.follow_unsel));
+                personal.setImageDrawable(getResources().getDrawable(R.mipmap.mine_unsel));
                 replaceFragment(new RecommendFragment());
                 break;
             case R.id.personal_item:
+                recommend.setImageDrawable(getResources().getDrawable(R.mipmap.recommed_unsel));
+                theme.setImageDrawable(getResources().getDrawable(R.mipmap.theme_unsel));
+                find.setImageDrawable(getResources().getDrawable(R.mipmap.follow_unsel));
+                personal.setImageDrawable(getResources().getDrawable(R.mipmap.mine_sel));
                 replaceFragment(new PersonalFragmenr());
                 break;
             case R.id.find_item:
+                recommend.setImageDrawable(getResources().getDrawable(R.mipmap.recommed_unsel));
+                theme.setImageDrawable(getResources().getDrawable(R.mipmap.theme_unsel));
+                find.setImageDrawable(getResources().getDrawable(R.mipmap.follow_sel));
+                personal.setImageDrawable(getResources().getDrawable(R.mipmap.mine_unsel));
                 replaceFragment(new FindFragment());
                 break;
         }
     }
 }
+//
+//class mTabLayout extends TabLayout {
+//    public mTabLayout(Context context) {
+//        super(context);
+//        mTabLayout.setOnTabSelectedListener(new TabLayoutOnTabSelectedListener() {
+//            @Override
+//            public void onTabSelected(Tab tab) {
+//
+//            }
+//
+//            @Override
+//            public void onTabUnselected(Tab tab) {
+//
+//            }
+//
+//            @Override
+//            public void onTabReselected(Tab tab) {
+//
+//            }
+//        })
+//    }
+//
+//
+//}
